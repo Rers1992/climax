b9bfd352037e3c45581c0d0bcc1a229225f9c8ac
Rodolfo Ruiz <rruiz.sazo@gmail.com>
2020-06-09 01:36:01 -0400

    indices listos para revisión

--- apps/estacion/indicadores.py
+++ apps/estacion/indicadores.py
@@ -1,15 +1,15 @@
 from apps.memoria.models import MemAno, MemMes
 import math
 
-def funcion_percentil_10(datos):
-      datos2 = datos.sort('temperaturaminserie')
+def funcion_percentil(datos, percentil, columna):
+      datos2 = datos.sort(columna)
       n = len(datos2)
-      i = n * 0.1
+      i = n * percentil
       if(i % 1 == 0):
-          p = (datos2['temperaturaminserie'][int(i)] + datos2['temperaturaminserie'][int(i-1)])/2
+          p = (datos2[columna][int(i)] + datos2[columna][int(i-1)])/2
           return p
       else:
-          return datos2['temperaturaminserie'][math.ceil(i)]
+          return datos2[columna][math.ceil(i-1)]
 
 def anos_meses(data2):
     existeAno = MemAno.objects.filter(ano = data2[0])
@@ -36,8 +36,6 @@ def funcion_cdd(precipitacionserie, largoExcel, cddCount, ciclo):
              cddCount[0] += 1
          if(cddCount[1] < cddCount[0]):
              cddCount[1] = cddCount[0]
-             cddCount[0] = 0
-             return cddCount
          cddCount[0] = 0
          return cddCount
 
@@ -50,8 +48,6 @@ def funcion_csdi(temperaturaminserie, largoExcel, csdiCount, ciclo, percentil_10
              csdiCount[0] += 1
          if(csdiCount[1] < csdiCount[0]):
              csdiCount[1] = csdiCount[0]
-             csdiCount[0] = 0
-             return csdiCount
          csdiCount[0] = 0
          return csdiCount
 
@@ -64,8 +60,6 @@ def funcion_cwd(precipitacionserie, largoExcel, cwdCount, ciclo):
              cwdCount[0] += 1
          if(cwdCount[1] < cwdCount[0]):
              cwdCount[1] = cwdCount[0]
-             cwdCount[0] = 0
-             return cwdCount
          cwdCount[0] = 0
          return cwdCount
 
@@ -88,3 +82,90 @@ def funcion_prcptot(precipitacion,prcptot):
     if(precipitacion >= 1):
         prcptot += precipitacion
     return prcptot
+
+def funcion_r95p(precipitacion, r95p, percentil95):
+    if(precipitacion > percentil95):
+        r95p += precipitacion
+    return r95p
+
+def funcion_r99p(precipitacion, r99p, percentil99):
+    if(precipitacion > percentil99):
+        r99p += precipitacion
+    return r99p
+
+def funcion_rx1day(datos):
+    datos.sort('precipitacionserie')
+    return datos['precipitacionserie'][0]
+
+def funcion_sdii(precipitacion, sdii):
+    if(precipitacion >= 1):
+        sdii[0] += 1
+        sdii[1] += precipitacion
+    return sdii
+
+def funcion_su25(temMaxima, su25):
+    if(temMaxima > 25):
+        su25 += 1
+    return su25
+
+def funcion_tn10p(percentil_10, temMinima, ciclo, largo, tn10p):
+    if(temMinima < percentil_10 and ciclo != largo):
+        tn10p += 1
+    elif(ciclo == largo):
+        if(temMinima < percentil_10):
+            tn10p += 1
+        tn10p = (tn10p * 100)/largo
+    return tn10p
+
+def funcion_tn90p(percentil_90, temMinima, ciclo, largo, tn90p):
+    if(temMinima > percentil_90 and ciclo != largo):
+        tn90p += 1
+    elif(ciclo == largo):
+        if(temMinima > percentil_90):
+            tn90p += 1
+        tn90p = (tn90p * 100)/largo
+    return tn90p
+
+def funcion_tnn(datos):
+    datos2= datos.sort('temperaturaminserie')
+    return datos2['temperaturaminserie'][0] 
+
+def funcion_txn(datos):
+    datos2= datos.sort('temperaturamaxserie')
+    return datos2['temperaturamaxserie'][0]
+
+def funcion_tx10p(percentil_10, temMaxima, ciclo, largo, tx10p):
+    if(temMaxima < percentil_10 and ciclo != largo):
+        tx10p += 1
+    elif(ciclo == largo):
+        if(temMaxima < percentil_10):
+            tx10p += 1
+        tx10p = (tx10p * 100)/largo
+    return tx10p
+
+def funcion_tx90p(percentil_90, temMaxima, ciclo, largo, tx90p):
+    if(temMaxima > percentil_90 and ciclo != largo):
+        tx90p += 1
+    elif(ciclo == largo):
+        if(temMaxima > percentil_90):
+            tx90p += 1
+        tx90p = (tx90p * 100)/largo
+    return tx90p
+
+def funcion_tnx(datos):
+    datos2= datos.sort('temperaturaminserie')
+    return datos2['temperaturaminserie'][len(datos2)-1] 
+
+def funcion_txx(datos):
+    datos2= datos.sort('temperaturamaxserie')
+    return datos2['temperaturamaxserie'][len(datos2)-1]
+
+def funcion_wsdi(percentil_90, temMax, wsdi):
+    if(percentil_90 < temMax):
+        wsdi[0] += 1
+    elif(percentil_90 > temMax and wsdi[0] != 6):
+        wsdi[0] = 0
+    if(wsdi[0] == 6):
+        wsdi[1] += 1
+        wsdi[0] = 0
+    return wsdi
--- apps/estacion/views.py
+++ apps/estacion/views.py
@@ -32,10 +32,22 @@ def importarEstacion(request, codigoEstacion):
      nuevas_anos = request.FILES['xlsfile'] 
      imported_data = dataset.load(nuevas_anos.read())
      contador = 0; ciclo = 0; largo = len(imported_data)
-     cddCount = [0, 0]; csdiCount = [0, 0]; cwdCount= [0, 0]; fd0 = 0; id0 = 0; prcptot = 0
+     cddCount = [0, 0]; csdiCount = [0, 0]; cwdCount= [0, 0]; fd0 = 0; id0 = 0; prcptot = 0; r95p = 0; r99p = 0
+     sdiiCount = [0,0]; su25 = 0; tn10p = 0; tn90p = 0; tx10p = 0; tx90p = 0; wsdi = [0,0]
      temperaturasMaxMin = [0,0]
-     percentil_10 = funcion_percentil_10(imported_data)
-     #me salte el indicador 6
+     percentil_10 = funcion_percentil(imported_data, 0.1, 'temperaturaminserie')
+     percentil_10TemMAX = funcion_percentil(imported_data, 0.1, 'temperaturamaxserie')
+     percentil_90 = funcion_percentil(imported_data, 0.9, 'temperaturaminserie')
+     percentil_90TemMax = funcion_percentil(imported_data, 0.9, 'temperaturamaxserie')
+     percentil_95 = funcion_percentil(imported_data, 0.95, 'precipitacionserie')
+     percentil_99 = funcion_percentil(imported_data, 0.99, 'precipitacionserie')
+     rx1day = funcion_rx1day(imported_data)
+     tnn = funcion_tnn(imported_data)
+     txn = funcion_txn(imported_data)
+     tnx = funcion_tnx(imported_data)
+     txx = funcion_txx(imported_data)
+     #me salte el indicador 6, 15, 22
+     #6, 7, 13, 22 no salen
      for x in imported_data:
         imported_data['codigoestacion'][contador] = codigoEstacion
         ciclo += 1
@@ -46,10 +58,20 @@ def importarEstacion(request, codigoEstacion):
         fd0 = funcion_fd0(imported_data['temperaturaminserie'][contador], fd0)
         id0 = funcion_id0(imported_data['temperaturamaxserie'][contador], id0)
         prcptot = funcion_prcptot(imported_data['precipitacionserie'][contador], prcptot)
+        r95p = funcion_r95p(imported_data['precipitacionserie'][contador], r95p, percentil_95)
+        r99p = funcion_r99p(imported_data['precipitacionserie'][contador], r99p, percentil_99)
+        sdiiCount = funcion_sdii(imported_data['precipitacionserie'][contador], sdiiCount)
+        su25 = funcion_su25(imported_data['temperaturamaxserie'][contador], su25)
+        tn10p = funcion_tn10p(percentil_10, imported_data['temperaturaminserie'][contador], ciclo, largo, tn10p)
+        tn90p = funcion_tn90p(percentil_90, imported_data['temperaturaminserie'][contador], ciclo, largo, tn90p)
+        tx10p = funcion_tx10p(percentil_10TemMAX, imported_data['temperaturamaxserie'][contador], ciclo, largo, tx10p)
+        tx90p = funcion_tx90p(percentil_90TemMax, imported_data['temperaturamaxserie'][contador], ciclo, largo, tx90p)
+        wsdi = funcion_wsdi(percentil_90TemMax, imported_data['temperaturamaxserie'][contador], wsdi)
         data2 = imported_data['fechaserie'][contador].split('-')
         anos_meses(data2)
         contador += 1
      dtr = (temperaturasMaxMin[0] - temperaturasMaxMin[1])/largo
+     sdii = sdiiCount[1]/sdiiCount[0]
      print(cddCount[1])
      print(csdiCount[1])
      print(cwdCount[1])
@@ -57,6 +79,20 @@ def importarEstacion(request, codigoEstacion):
      print(fd0)
      print(id0)
      print(prcptot)
+     print(r95p)
+     print(r99p)
+     print(rx1day)
+     print(sdii)
+     print(su25)
+     print(tn10p)
+     print(tn90p)
+     print(tnn)
+     print(txn)
+     print(tx10p)
+     print(tx90p)
+     print(tnx)
+     print(txx)
+     print(wsdi[1])
      result = serie_resource.import_data(dataset, dry_run=True) # Test the data import
      if not result.has_errors():  
        serie_resource.import_data(dataset, dry_run=False) # Actually import now
