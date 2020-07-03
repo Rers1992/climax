20c10981c69096d78ba1b84255ca78ac8d26de3f
Rodolfo Ruiz <rruiz.sazo@gmail.com>
2020-07-02 22:52:19 -0400

    dashboard

--- static/js/react.js
+++ static/js/react.js
@@ -1,7 +1,7 @@
-class LikeButton extends React.Component {
+class Dashboard extends React.Component {
     constructor(props){
         super(props)
-        this.chartRef = React.createRef()
+        this.myRef = React.createRef();
         this.state = {
             data: []
         }
@@ -88,6 +88,44 @@ class LikeButton extends React.Component {
     }
 
     componentDidMount() {
+      const ctx = this.ctx;
+
+      new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
         fetch('indices2')
           .then(response => response.json())
           .then(data => this.setState({ data: data.indices})
@@ -98,10 +136,11 @@ class LikeButton extends React.Component {
     return  <div>
      {this.renderBotonesAños(this.state.data)}   
      {this.renderTabla(this.state.data)}
+     <canvas width="400" height="400" ref={ctx => (this.ctx = ctx)}/>
      </div>;
   }
 }
 
 // Find all DOM containers, and render Like buttons into them.
-const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(<LikeButton/>, domContainer);
+const domContainer = document.querySelector('#react');
+ReactDOM.render(<Dashboard/>, domContainer);
